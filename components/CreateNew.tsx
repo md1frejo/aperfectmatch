9dd9d4da54adecf16ad6acf98d003a0b1803d8b7
md1frejo <jonas.mb@gmail.com>
2026-03-09 15:46:28 +0100

    fixed add profile

--- components/CreateNew.tsx
+++ components/CreateNew.tsx
@@ -5,36 +5,44 @@ import LoadPicture from "./LoadPicture"
 import SelectCrit from "@/components/SelectCriteria"
 
 type CreateNewProps = {
-  onCreate: (profile: { image: string; text: string }) => void
+  onCreate: (profile: { image: string; text: string[] }) => void
 }
 
 export default function CreateNew({ onCreate }: CreateNewProps) {
 
   const [image, setImage] = useState("")
-  const [text, setText] = useState("")
+  const [text, setText] = useState<string[]>([])
 
   const handleSubmit = () => {
 
-    if (!image || !text) return
+    if (!image || text.length === 0) return
 
     onCreate({ image, text })
     setImage("")
-    setText("")
+    setText([])
   }
 
   return (
     <div className="flex flex-row justify-center items-center gap-8 bg-blue-100">	  
-    <SelectCrit />
-    <div className="mb-8 p-4 border rounded">
-      <LoadPicture onImageChange={setImage} />
+      <SelectCrit />
 
-      <textarea placeholder="Write something about yourself..." value={text}
-        onChange={(e) => setText(e.target.value)} className="w-full p-2 border rounded mb-2"/>
+      <div className="mb-8 p-4 border rounded">
+        <LoadPicture onImageChange={setImage} />
 
-      <button onClick={handleSubmit} className="bg-black text-white px-4 py-2 rounded">
-        Create Profile
-      </button>
+        <textarea
+          placeholder="Write something about yourself..."
+          value={text.join("\n")}
+          onChange={(e) => setText(e.target.value.split("\n"))}
+          className="w-full p-2 border rounded mb-2"
+        />
+
+        <button
+          onClick={handleSubmit}
+          className="bg-black text-white px-4 py-2 rounded"
+        >
+          Create Profile
+        </button>
+      </div>
     </div>
-   </div> 
   )
 }
